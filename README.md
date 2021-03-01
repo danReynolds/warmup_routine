9f54e9585f2c71607897d5c45159cf8f021fc328
Dan Reynolds <me@danreynolds.ca>
2021-03-01 08:05:18 -0500

    add more examples

--- README.md
+++ README.md
@@ -62,4 +62,82 @@ class WarmupRoutineExample extends StatelessWidget {
 }
 ```
 
-Full example in [GitHub repo](https://github.com/danReynolds/warmup_routine/blob/master/example.dart)
+### Navigator example
+
+```
+// The overlay will remain on top of the application above navigation events, so Navigator.push/pop
+// can be warmed up as well:
+
+class NavigationWarmupScreen extends StatefulWidget {
+  final Function onComplete;
+
+  NavigationWarmupScreen({@required this.onComplete});
+
+  _NavigationWarmupScreenState createState() => _NavigationWarmupScreenState();
+}
+
+class _NavigationWarmupScreenState extends State<NavigationWarmupScreen> {
+  initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) {
+            return YourWidget();
+          },
+          fullscreenDialog: true,
+        ),
+      );
+
+      Future.delayed(
+        // Adjust duration as needed
+        Duration(milliseconds: 400),
+        () {
+          Navigator.pop(context);
+          Future.delayed(
+            Duration(milliseconds: 400),
+            () {
+              widget.onComplete();
+            },
+          );
+        },
+      );
+    });
+  }
+
+  @override
+  build(context) {
+    return Container();
+  }
+}
+
+class WarmupOverlayNavigationExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WarmupOverlay(
+      onComplete: () {
+        // Start rest of application
+      },
+      builder: (context) {
+        return Container(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+      animations: [
+        WarmupAnimation(
+          builder: (context, complete) {
+            return NavigationWarmupScreen(onComplete: complete);
+          },
+          repeat: 2,
+        ),
+      ],
+    );
+  }
+}
+```
+
+Full examples in [GitHub repo](https://github.com/danReynolds/warmup_routine/blob/master/example.dart)
--- example.dart
+++ example.dart
@@ -72,6 +72,7 @@ class _OpenContainerAnimationState extends State<OpenContainerAnimation> {
   }
 }
 
+// Example of showing an overlay under which the animations are executing.
 class WarmupOverlayExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -99,6 +100,8 @@ class WarmupOverlayExample extends StatelessWidget {
   }
 }
 
+// If an overlay is not desired, warmup animations can be executed directly
+// with the WarmupRoutine widget
 class WarmupRoutineExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -115,6 +118,80 @@ class WarmupRoutineExample extends StatelessWidget {
       onComplete: () {
         // Start rest of application
       },
+    );
+  }
+}
+
+// The overlay will remain on top of the application above navigation events, so Navigator.push/pop
+// can be warmed up as well:
+
+class NavigationWarmupScreen extends StatefulWidget {
+  final Function onComplete;
+
+  NavigationWarmupScreen({@required this.onComplete});
+
+  _NavigationWarmupScreenState createState() => _NavigationWarmupScreenState();
+}
+
+class _NavigationWarmupScreenState extends State<NavigationWarmupScreen> {
+  initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) {
+            return YourWidget();
+          },
+          fullscreenDialog: true,
+        ),
+      );
+
+      Future.delayed(
+        // Adjust duration as needed
+        Duration(milliseconds: 400),
+        () {
+          Navigator.pop(context);
+          Future.delayed(
+            Duration(milliseconds: 400),
+            () {
+              widget.onComplete();
+            },
+          );
+        },
+      );
+    });
+  }
+
+  @override
+  build(context) {
+    return Container();
+  }
+}
+
+class WarmupOverlayNavigationExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WarmupOverlay(
+      onComplete: () {
+        // Start rest of application
+      },
+      builder: (context) {
+        return Container(
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+      animations: [
+        WarmupAnimation(
+          builder: (context, complete) {
+            return NavigationWarmupScreen(onComplete: complete);
+          },
+          repeat: 2,
+        ),
+      ],
     );
   }
 }
